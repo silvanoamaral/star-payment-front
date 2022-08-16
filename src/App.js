6670b9c6a00a9cda9026b606108346caa753a99e
silvanoamaral <silvano.amaral9@gmail.com>
2022-08-16 14:38:05 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [dataEvent, setDataEvent] = useState(null);
+  const [listening, setListening] = useState(false);
+
+  useEffect(() => {
+    if (!listening) {
+      const events = new EventSource(
+        "https://projeto-ssr-example.herokuapp.com/events"
+      );
+
+      events.onmessage = (event) => {
+        setDataEvent(event.data);
+      };
+
+      setListening(true);
+    }
+  }, [listening]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Dados do cartão</h3>
+      <iframe
+        src="https://projeto-ssr-example.herokuapp.com/server-client"
+        title="An iframe with options of payment"
+        height="100%"
+      ></iframe>
+
+      <strong>server sent events: {dataEvent}</strong>
     </div>
   );
 }
