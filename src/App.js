314f05c0433bcdb2f301a294d3a6fb95e138e5bb
Alexandra Alcantara <alexandra.alcantara@grupoboticario.com.br>
2022-08-17 16:07:15 -0300

    ✨ Add button Continuar para pagamento

--- src/App.js
+++ src/App.js
@@ -1,34 +1,56 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 
+const eventsPrd = "https://projeto-ssr-example.herokuapp.com/events";
+// const eventsLocal = "http://localhost:8000/events";
+
+const eventsEnvironment = eventsPrd;
+
+const iframePrd = "https://projeto-ssr-example.herokuapp.com/server-client";
+// const iframeLocal = "http://localhost:8000/server-client";
+
+const iframeEnvironment = iframePrd;
+
 function App() {
   const [dataEvent, setDataEvent] = useState(null);
   const [listening, setListening] = useState(false);
+  const [disabled, setDisabled] = useState(true);
 
   useEffect(() => {
     if (!listening) {
-      const events = new EventSource(
-        "https://projeto-ssr-example.herokuapp.com/events"
-      );
+      const events = new EventSource(eventsEnvironment);
 
       events.onmessage = (event) => {
         setDataEvent(event.data);
+      };
+
+      events.onerror = () => {
+        setDataEvent("Server closed connection");
+        events.close();
       };
 
       setListening(true);
     }
   }, [listening]);
 
+  useEffect(() => {
+    if (dataEvent) {
+      const data = JSON.parse(dataEvent);
+      setDisabled(data.isDisabled);
+    }
+  }, [dataEvent]);
+
   return (
     <div className="App">
       <h3>Dados do cartão</h3>
       <iframe
-        src="https://projeto-ssr-example.herokuapp.com/server-client"
+        src={iframeEnvironment}
         title="An iframe with options of payment"
-        height="100%"
+        height="90%"
       ></iframe>
-
-      <strong>server sent events: {dataEvent}</strong>
+      <button className="button" disabled={disabled}>
+        Continuar para pagamento
+      </button>
     </div>
   );
 }
